090b9188b0fb9a2f12c34674a2770a531202d5ac
Dardan Asllani <asllanidardan@gmail.com>
2024-09-02 19:46:25 +0200

    added cors configs

--- functions/api.js
+++ functions/api.js
@@ -1,15 +1,51 @@
-exports.handler = async function(event, context) {
-  return {
-    statusCode: 200,
-    headers: {
-        "Content-Type": "application/json"
-    },
-    body: JSON.stringify({
-      message: "This is static JSON data",
-      data: {
-        key: "value",
-        status: "success"
-      }
-    })
-  };
+const issues = require("./issues.json");
+
+exports.handler = async function(event) {
+  // Parse the HTTP method from the incoming request
+  const httpMethod = event.httpMethod;
+  
+  // Handle different HTTP methods
+  switch(httpMethod) {
+    case "GET":
+      return {
+        statusCode: 200,
+        body: JSON.stringify({
+          message: "Read operation - static JSON returned",
+          data: issues
+        })
+      };
+      
+      case "POST":
+        return {
+          statusCode: 200,
+          body: JSON.stringify({
+            message: "Create operation - received JSON object",
+            receivedData: JSON.parse(event.body)
+          })
+        };
+
+      case "PUT":
+        return {
+          statusCode: 200,
+          body: JSON.stringify({
+            message: "Update operation - received JSON object",
+            receivedData: JSON.parse(event.body)
+          })
+        };
+
+      case "DELETE":
+        return {
+          statusCode: 200,
+          body: JSON.stringify({
+            message: "Delete operation - received data",
+            dataToDelete: event.queryStringParameters
+          })
+        };
+
+      default:
+        return {
+          statusCode: 405,
+          body: JSON.stringify({ error: "Method Not Allowed" })
+        };
+  }
 };
