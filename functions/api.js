02dbdaf6b684aa0fd830e3c0684006666da158a1
Dardan Asllani <asllanidardan@gmail.com>
2024-09-02 19:53:30 +0200

    returning CORS headers from function

--- functions/api.js
+++ functions/api.js
@@ -1,6 +1,20 @@
 const issues = require("./issues.json");
 
 exports.handler = async function(event) {
+  // Set CORS headers
+  const headers = {
+    "Access-Control-Allow-Origin": "*", // This allows all domains. For production, specify your domain instead.
+    "Access-Control-Allow-Headers": "Content-Type",
+    "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS"
+  };
+  
+  // Check for OPTIONS request (CORS preflight)
+  if (event.httpMethod === "OPTIONS") {
+    return {
+      statusCode: 204,
+      headers
+    };
+  }
   // Parse the HTTP method from the incoming request
   const httpMethod = event.httpMethod;
   
